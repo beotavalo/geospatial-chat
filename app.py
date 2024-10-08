245c0dea68c46acbf1c6f7332c23c845dfb0df03
Braulio Otavalo <56369660+beotavalo@users.noreply.github.com>
2024-10-08 15:56:32 +0000

    RAG completed

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import pandas as pd
 import streamlit as st
 import pandas as pd
 import numpy as np
+from typing import List
 import src.utils as ut
 import src.llm_api as llm
 import pinecone
@@ -48,44 +49,85 @@ def readData(data_path):
     except FileNotFoundError:
         raise Exception("The 'train_data.csv' file was not found within folder 'data/'.")
     return df
+def unified_store_podcast_vectors(df: pd.DataFrame):
 
-def store_podcast_vectors_in_pinecone(df):
-    # Cargar modelo de embeddings
+    # Load embeddings model
     model = SentenceTransformer("all-mpnet-base-v2")
 
-    # Concatenar título y descripción
+    # Concatenate  Title and Description
     df['document'] = df['question'] + " " + df['text']
-    
-    # Generar embeddings
-    kb_vectors = model.encode(df['document'], batch_size=16, show_progress_bar=True)
 
-        # Almacenar los vectores en Pinecone
-    for idx, vector in enumerate(kb_vectors):
-        pinecone_id = str(df['id'].iloc[idx])
-        question = str(df['question'].iloc[idx])
-        area = str(df['area'].iloc[idx])
-        text = str(df['text'].iloc[idx])
-        index.upsert(vectors=[{"id": pinecone_id,"values": vector,"metadata": {"question": question, "area": area, 'text':text}}])
+    # Process batch registers
+    def process_batch(batch: List[pd.Series]):
+        ids_to_check = [str(row["id"]) for row in batch]
+        try:
+            existing_vectors = index.fetch(ids=ids_to_check)["vectors"]
+        except Exception as e:
+            print(f"Error to get existent vectors: {e}")
+            existing_vectors = {}
 
+        new_vectors = []
+        for row in batch:
+            pinecone_id = str(row["id"])
+            if pinecone_id not in existing_vectors:
+                vector = model.encode(row["document"])
+                new_vectors.append(
+                    {
+                        "id": pinecone_id,
+                        "values": vector.tolist(),
+                        "metadata": {
+                            "question": str(row["question"]), 
+                            "area": str(row["area"]),
+                            "text": str(row["text"]),
+                        },
+                    }
+                )
+            else:
+                print(
+                    f"Vector with ID {pinecone_id} already exists on Pinecone")
 
-    print("Vector DB created on Pinecone")
+        if new_vectors:
+            try:
+                index.upsert(vectors=new_vectors)
+                print(f"The {len(new_vectors)} upserted on Pinecone")
+            except Exception as e:
+                print(f"Error storaging new vectors: {e}")
+
+    # Procesar el DataFrame en lotes
+    batch_size = 100  # Ajusta este valor según tus necesidades
+    for i in range(0, len(df), batch_size):
+        batch = df.iloc[i: i + batch_size]
+        process_batch(batch.to_dict("records"))
+
+    print("Completed process")
 
 # Función para recuperar el contexto relevante
 def get_context_from_pinecone(user_query):
     model = SentenceTransformer("all-mpnet-base-v2")
-    query_vector = model.encode(user_query)  # Función que embebe la query
-    results = index.query(queries=[query_vector], top_k=5)
-    context = " ".join([result['text'] for result in results['matches']])
-    return context
 
+    
+    query_vector = model.encode(user_query).tolist()  
+    results = index.query(vector=query_vector, top_k=1, include_metadata=True)
+
+    context = []
+    for match in results["matches"]:
+        # Check if 'metadata' exist and y contains 'title'
+        if "metadata" in match and "text" in match["metadata"]:
+            context.append(match["metadata"]["text"])
+        else:
+            # in case the 'title' doesn exist, use the ID as fallback
+            context.append(f"Document ID: {match['id']}")
+
+    
+    return context
+   
 
 #Main
-
 def main():
     data_path = '/workspaces/geospatial-chat/data/processed/train_data.csv'
     df = readData(data_path )
-    store_podcast_vectors_in_pinecone(df)
-
+    unified_store_podcast_vectors(df)
+   
     st.title("Geospatial Chat - ACME")
     user_query = st.text_input("Enter your query")
     if user_query:
@@ -96,11 +138,11 @@ def main():
         prompt = ut.create_prompt(user_query, context)
     
         # Enviar el prompt al modelo Llama
-        #response = llm.call_llama_model(prompt)
-        response = 'Development my response'
+        response = llm.call_llm_apenai_model(prompt)
+        #response = 'Development my response'
 
         #Show RAG answer
-        st.write(f"Model Response: {response}")
+        st.write(f"Response: {response}")
 
 if __name__ == "__main__":
     main()
--- src/llm_api.py
+++ src/llm_api.py
@@ -1,5 +1,18 @@
 import os
 import requests
+from openai import OpenAI
+import src.utils as ut
+openai_api_key = ut.get_openai_api_key()
+client = OpenAI(api_key=openai_api_key)
+
+
+def call_llm_apenai_model(prompt):
+    response = client.chat.completions.create(
+        model='gpt-3.5-turbo',
+        messages=[{"role": "user", "content": prompt}]
+    )
+    
+    return response.choices[0].message.content
 
 def call_llm_model(prompt):
     # Supongamos que tienes una API corriendo para Llama 3.1
